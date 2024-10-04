5c98244c54ff67daaeb8e3b72ca26a1c42a2df6a
mrtnmndc <martin.mondaca2023@twk.cl>
2024-10-04 11:04:45 -0300

    creacion formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component'; // Asegúrate de que la ruta es correcta
-import { FooterComponent } from './footer/footer.component'; // Asegúrate de que la ruta es correcta
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, CommonModule, NavbarComponent, FooterComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
